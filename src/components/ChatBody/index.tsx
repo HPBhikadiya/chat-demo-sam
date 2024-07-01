cc54e9b9b2145fad7db7df19fa0ffcfbdfaa82b8
HPBhikadiya <hirenpbhikadiya@gmail.com>
2024-07-01 17:41:42 +0530

    skew added

--- src/components/ChatBody/index.tsx
+++ src/components/ChatBody/index.tsx
@@ -112,22 +112,18 @@ const ChatMessage = ({ message }: { message: any }) => {
           alt={`${message.name} avatar`}
         />
       )}
-      <div className="max-w-xs">
+      <div className="max-w-xs relative">
         <div
-          className={`p-2 rounded-lg ${
+          className={`p-2 rounded-lg  ${
             message.type === "sent"
-              ? "bg-blue-500 text-white"
-              : "bg-gray-100 text-gray-900"
-          } ml-4 text-sm p-2 rounded-lg inline-block`}
-          style={{
-            // transformOrigin: "top left",
-            // transform: "skew(-10deg)",
-          }}
+              ? "bg-blue-500 text-white rounded-tr-none"
+              : "bg-gray-100 text-gray-900 rounded-tl-none"
+          } ml-4 text-sm p-2 rounded-lg inline-block w-[calc(100%-8px)]`}
         >
           <div className="flex pb-1.5 items-center">
             {message.type === "received" && (
               <span className="text-xs text-gray-500 font-bold">
-                {message.name}{" "}
+                {message.name}
                 <span className="font-normal">{message.role}</span>
               </span>
             )}
@@ -146,6 +142,74 @@ const ChatMessage = ({ message }: { message: any }) => {
             </span>
           </div>
         </div>
+        {message.type === "sent" ? (
+          <span
+            aria-hidden="true"
+            data-icon="tail-out"
+            className={`absolute top-0 z-layer-1 block  h-13 ${
+              message.type === "sent"
+                ? "right-[-15px] "
+                : "left-[10px] transform translate-[120deg]"
+            }`}
+          >
+            <svg
+              viewBox="0 0 8 13"
+              height="13"
+              width="8"
+              preserveAspectRatio="xMidYMid meet"
+              className=""
+              version="1.1"
+              x="0px"
+              y="0px"
+              color={"#3b82f6"}
+              enable-background="new 0 0 8 13"
+            >
+              <title>tail-out</title>
+              <path
+                opacity="0.13"
+                d="M5.188,1H0v11.193l6.467-8.625 C7.526,2.156,6.958,1,5.188,1z"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M5.188,0H0v11.193l6.467-8.625C7.526,1.156,6.958,0,5.188,0z"
+              ></path>
+            </svg>
+          </span>
+        ) : (
+          <span
+            aria-hidden="true"
+            data-icon="tail-out"
+            className={`absolute top-0 z-layer-1 block  h-13 ${
+              message.type === "sent"
+                ? "right-[-15px] "
+                : "left-[10px] transform translate-[120deg]"
+            }`}
+          >
+            <svg
+              viewBox="0 0 8 13"
+              height="13"
+              width="8"
+              preserveAspectRatio="xMidYMid meet"
+              className=""
+              version="1.1"
+              color="#f3f4f6"
+              x="0px"
+              y="0px"
+              enable-background="new 0 0 8 13"
+            >
+              <title>tail-in</title>
+              <path
+                opacity="0.13"
+                fill="#0000000"
+                d="M1.533,3.568L8,12.193V1H2.812 C1.042,1,0.474,2.156,1.533,3.568z"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M1.533,2.568L8,11.193V0L2.812,0C1.042,0,0.474,1.156,1.533,2.568z"
+              ></path>
+            </svg>
+          </span>
+        )}
       </div>
       {message.type === "sent" && (
         <img
